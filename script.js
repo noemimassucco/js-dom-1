4dea201abc8f001efb2efcd305e0f285b375c0dd
NoemiMassucco <noemimassucco@gmail.com>
2025-12-25 23:12:14 +0100

    Aggiunto script, ma lampadina non si accende

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //Inseriamo in pagina l’immagine della lampadina spenta 
 // che trovate in allegato e accanto un bottone con la scritta “Accendi”.
+const button = document.getElementById('toggleButton');
+const lampImage = document.getElementById('lampImage');
 
+button.addEventListener('click', function() {
+    lampImage.src = '/img/yellow_lamp.png'; // Cambia l'immagine della lampadina accesa
+}); 
 //Al click del bottone, la lampadina dovrà accendersi 
 
 
